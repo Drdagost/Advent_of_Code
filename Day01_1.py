b77b9342d0b8edf724a31bb1a15774ffe9337be2
Drdagost <davin15488@gmail.com>
2020-12-03 07:56:49 -0500

    second commit

---
+++ Day01_1.py
@@ -0,0 +1,19 @@
+import itertools
+
+with open('Day01_input.txt') as f:
+    integer_array = []
+    for line in f:
+        integer_array.append(int(line.rstrip('\n')))
+
+#integer_array = [1721, 979, 366, 299, 675, 1456]
+
+target = 2020
+product = 1
+for numbers in itertools.combinations(integer_array,2):
+    if sum(numbers) == target:
+        pair = [integer_array.index(number) for number in numbers]
+        
+        print("The numbers are " + str(numbers))
+        for number in numbers: 
+            product *= number
+        print("Product = " + str(product))
